51e5760cb082afccfecc619c045199bc69b69e2d
Magdaléna Pazlarová <m.pazlarova@gmail.com>
2021-05-29 10:47:39 +0200

    router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/reservation">
+          <Reservation />
+        </Route>
+      </Switch>
+    </Router>
+
     <Footer />
   </>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Reservation = () => {
+  return (
+    <>
+      <h2>Rezervace</h2>
+    </>
+  );
+};
